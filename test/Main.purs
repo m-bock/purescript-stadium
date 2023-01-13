7115a33f442149e18c67486994d7e35965b160d5
Michael Bock <no-day@posteo.net>
2023-01-13 07:24:58 +0100

    Tabula rasa

--- test/Main.purs
+++ test/Main.purs
@@ -6,14 +6,7 @@ import Effect (Effect)
 import Effect.Aff (launchAff_)
 import Test.Spec.Reporter.Console (consoleReporter)
 import Test.Spec.Runner (runSpec)
-import Test.Stadium.Reflection as Test.Stadium.Reflection
-import Test.Stadium.DotGraph as Test.Stadium.DotGraph
-import Test.TestReadme as Test.TestReadme
-
-
 
 main :: Effect Unit
 main = launchAff_ $ runSpec [ consoleReporter ] do
-  Test.Stadium.Reflection.spec
-  Test.Stadium.DotGraph.spec
-  Test.TestReadme.spec
+  pure unit
